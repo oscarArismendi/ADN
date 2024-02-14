945a32a62b4cf384938468528598cb019b1ad131
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-02-13 22:04:08 -0500

    update css index and js

--- js/script.js
+++ js/script.js
@@ -4,18 +4,47 @@ const adn = {
     "diego":"00100010010000001001"
 }
 
+function startADN(){
+    for(let key in adn){
+        let i = 1;
+        for(let character of adn[key]){
+            changeDiv = document.getElementById(key+(i+1))
+            changeDiv.style.background = "yellow";
+            console.log(changeDiv)
+            i++;
+        }
+    }
+    return false;
+}
+
 function search() {
+    // for(let key in adn){
+    //     let i = 1;
+    //     for(let character of adn[key]){
+    //         changeDiv = document.getElementById(key+(i+1))
+    //         console.log(changeDiv)
+    //         i++;
+    //     }
+    // }
     const inputADN = document.getElementById("input-adn").value;
-    // let nADN = adn.length;
-    // let n = Math.min(20,inputADN.length);
+    let n = Math.min(20,inputADN.length);
     let result = "none"
     let holderADN = -1;
     for(let key in adn){
         let holder = 0
         let i = 0;
         for(let character of adn[key]){
+            changeDiv = document.getElementById(key+(i+1));
+            if(i >= n){
+                // changeDiv.style.background = "yellow";
+                continue;
+            }
             if(character === inputADN[i]){
                 holder++;
+                changeDiv.style.background = "green";
+            }
+            else{
+                changeDiv.style.background = "red";
             }
             i++;
         }
@@ -31,3 +60,5 @@ function search() {
     document.getElementById("result").innerHTML = "result: "+result;
     event.preventDefault();
 }
+
+
